d8356a061ffd8d1d2eaf1c79333f43bc5de8877d
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-02-03 13:45:07 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-14_overview",
+      "name": "SF-14_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.005646891859154124,
+        "pitch": 0.5135525846105171,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SF-14",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
